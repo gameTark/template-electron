7d591680fbe0a03805c5e46a871e56a4459eafa7
gameTark <>
2024-06-07 05:22:30 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import electron from 'vite-plugin-electron/simple'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  plugins: [
+    react(),
+    electron({
+      main: {
+        // Shortcut of `build.lib.entry`
+        entry: 'src/app/main.ts',
+      },
+      preload: {
+        // Shortcut of `build.rollupOptions.input`
+        input: 'src/app/preload.ts',
+      },
+      // Optional: Use Node.js API in the Renderer process
+      renderer: {
+      },
+    }),
+
+  ],
+}
