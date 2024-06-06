e346bbeb24ca534c324e5a8048244a85e1389018
gameTark <>
2024-06-07 05:28:08 +0900

    setup

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </React.StrictMode>,
 )
